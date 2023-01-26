b95c80d47c3811c15fd189a84e052681377e9d42
Milena Napierała <m.napierala24@gmail.com>
2023-01-26 16:45:51 +0100

    step1

--- photosetup/photo_loader.py
+++ photosetup/photo_loader.py
@@ -1,6 +1,13 @@
+import imghdr
 
 def is_file_image(file_path: str) -> bool:
     # 1. Load a file from a file_path
+    # file_image = open(file_path)
     # 2. Check file type
+    detected_image_type = imghdr.what(file_path)
+    if detected_image_type is None:
+        return False
+    if type(detected_image_type) == str:
+        return True
     # 3. If type == image return True else return False
-    pass
+
