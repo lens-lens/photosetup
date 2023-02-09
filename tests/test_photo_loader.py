ee3d10d1dc83946de923f0eb4d9cf3e39e59c453
Milena Napierała <m.napierala24@gmail.com>
2023-02-09 16:37:18 +0100

    file path change

--- tests/test_photo_loader.py
+++ tests/test_photo_loader.py
@@ -1,7 +1,7 @@
 from photosetup import photo_loader
 
 def test_is_file_image_true():
-    test_file = "/Users/milenanapierala/Desktop/PROJECTS/photosetup/tests/image.png"
+    test_file = "/Users/milenanapierala/Desktop/PROJECTS/photosetup/tests/Jacek_red-9 75dpi.jpg"
     expected_result = True
     result = photo_loader.is_file_image(test_file)
     assert(result == expected_result)
