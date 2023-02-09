9af67119286e6276f4170574c38554854eeeb3ba
Milena Napierała <m.napierala24@gmail.com>
2023-02-09 17:05:58 +0100

    test show iso

--- tests/test_photo_loader.py
+++ tests/test_photo_loader.py
@@ -11,3 +11,9 @@ def test_is_file_image_false():
     expected_result = False
     result = photo_loader.is_file_image(test_file)
     assert(result == expected_result)
+
+def test_show_iso():
+    test_file = "/Users/milenanapierala/Desktop/PROJECTS/photosetup/tests/Jacek_red-9 75dpi.jpg"
+    expected_result = 100
+    result = photo_loader.show_iso(test_file)
+    assert(result == expected_result)
