36b03407b8b85997c0d1acebe0cf63aca07b2c6d
Milena Napierała <m.napierala24@gmail.com>
2023-02-09 17:23:19 +0100

    change metadatas

--- photosetup/photo_loader.py
+++ photosetup/photo_loader.py
@@ -12,15 +12,18 @@ def is_file_image(file_path: str) -> bool:
     if type(detected_image_type) == str:
         return True
 
-def show_iso(file_path: str) -> int: 
+def show_metadatas(file_path: str) -> int: 
     img = Image.open("/Users/milenanapierala/Desktop/PROJECTS/photosetup/tests/Jacek_red-9 75dpi.jpg")
     exif = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
-    return exif["ISOSpeedRatings"]
+    if exif is None:
+        logger.warning('Obrazek nie ma podanych metadanych')
+        return None
+    return exif
 
 # ponizszy kod nie wykona się w nowym .py przy importowaniu tego pliku .py
 if __name__ ==  "__main__":
     a = '/Users/milenanapierala/Desktop/PROJECTS/photosetup/tests/Jacek_red-9 75dpi.jpg' 
     result = is_file_image(a)
     print(result)
-    show = show_iso(a)
+    show = show_metadatas(a)
     print(show)
